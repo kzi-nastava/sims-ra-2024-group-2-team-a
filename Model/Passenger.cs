a84b59cc57582c5bf1eda0d27f129c03b6f2e7eb
Ostojic FTN <ostojic.ra27.2021@uns.ac.rs>
2024-03-10 23:16:49 +0100

    fix: Small fix to Passenger class
    - changed Username and Password to Name and Surname

--- Model/Passenger.cs
+++ Model/Passenger.cs
@@ -9,30 +9,30 @@ namespace BookingApp.Model {
     public class Passenger : ISerializable {
         public int Id { get; set; }
         public int TourId { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
         public int Age { get; set; }
 
         public Passenger() { }
 
-        public Passenger(int id, int tourId, string username, string password, int age) {
+        public Passenger(int id, int tourId, string name, string surname, int age) {
             Id = id;
             TourId = tourId;
-            Username = username;
-            Password = password;
+            Name = name;
+            Surname = surname;
             Age = age;
         }
 
         public string[] ToCSV() {
-            string[] csvValues = { Id.ToString(), TourId.ToString(), Username, Password, Age.ToString() };
+            string[] csvValues = { Id.ToString(), TourId.ToString(), Name, Surname, Age.ToString() };
             return csvValues;
         }
 
         public void FromCSV(string[] values) {
             Id = Convert.ToInt32(values[0]);
             TourId = Convert.ToInt32(values[1]);
-            Username = values[2];
-            Password = values[3];
+            Name = values[2];
+            Surname = values[3];
             Age = Convert.ToInt32(values[4]);
         }
     }
