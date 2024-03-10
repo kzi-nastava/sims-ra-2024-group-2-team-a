5403b42c8243d3e852988cef391c9ca39d8f7670
nkusla <nikola.kuslakovic1616@gmail.com>
2024-03-10 18:57:57 +0100

    feat: Added Guest class
    - Guest class inherits User class

---
+++ Model/Guest.cs
@@ -0,0 +1,31 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model {
+
+    public class Guest : User, ISerializable {
+
+        public bool isSuperGuest { get; set; }
+
+        public Guest() { }
+
+        public Guest(string username, string password) : base(username, password) {
+            Category = UserCategory.Guest;
+            isSuperGuest = false;
+        }
+
+        public string[] ToCSV() {
+            string[] csvValues = { Id.ToString(), isSuperGuest.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values) {
+            base.FromCSV(values);
+            isSuperGuest = Convert.ToBoolean(values[^1]);
+        }
+    }
+}
