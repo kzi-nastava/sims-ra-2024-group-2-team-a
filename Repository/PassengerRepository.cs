774ac25e7bca95282a1504e460d4be541f99b0b7
lostojicc <ostojiclukaftn@gmail.com>
2024-03-11 11:29:14 +0100

    feat: Added PassengerRepository and finished main Passenger layout
    - finished main information in Passenger class
    - Repository only added, methods yet to be implemented

---
+++ Repository/PassengerRepository.cs
@@ -0,0 +1,38 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookingApp.Repository {
+    public class PassengerRepository : IRepository<User> {
+        private const string FilePath = "../../../Resources/Data/users.csv";
+        private readonly Serializer<User> _serializer;
+
+        private List<User> _users;
+
+        public PassengerRepository() {
+            _serializer = new Serializer<User>();
+            _users = _serializer.FromCSV(FilePath);
+        }
+
+        public List<User> GetAll() {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(User item) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(User item) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(User item) {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetById(int id) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
