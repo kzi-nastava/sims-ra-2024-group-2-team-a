f5d09b4a9e89a98d8f6244d42ef14c8836dfc0f7
HakTak <katek.m536@gmail.com>
2024-03-11 11:50:50 +0100

    feat: Added model, repository and DTO for tour

---
+++ DTO/TourDTO.cs
@@ -0,0 +1,114 @@
+﻿using BookingApp.Model;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BookingApp.DTO {
+    public class TourDTO : INotifyPropertyChanged {
+        public TourDTO() {
+
+        }
+
+        public TourDTO(string name, int locationId, string description, string language, int maxTouristNumber, double duration, int currentTouristNumber, List<string> profilePictures) {
+            Name = name;
+            LocationId = locationId;
+            Description = description;
+            Language = language;
+            MaxTouristNumber = maxTouristNumber;
+            Duration = duration;
+            CurrentTouristNumber = currentTouristNumber;
+            ProfilePictures = profilePictures;
+        }
+
+        public TourDTO(Tour t) {
+            Name = t.Name;
+            LocationId = t.LocationId;
+            Description = t.Description;
+            Language = t.Language;
+            MaxTouristNumber = t.MaxTouristNumber;
+            Duration = t.Duration;
+            CurrentTouristNumber = t.CurrentTouristNumber;
+            ProfilePictures = t.ProfilePictures;
+        }
+
+        private string _name;
+        public string Name {
+            get {
+                return _name;
+            }
+            set {
+                if (_name != value) {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _locationId;
+        public int LocationId {
+            get { return _locationId; }
+            set {
+                if (_locationId != value) {
+                    _locationId = value; OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _description;
+        public string Description {
+            get { return _description; }
+            set {
+                if (_description != value) {
+                    _description = value; OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _language;
+        public string Language {
+            get { return _language; }
+            set {
+                if (_language != value) {
+                    _language = value; OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _maxTouristNumber;
+        public int MaxTouristNumber {
+            get { return _maxTouristNumber; }
+            set {
+                if (value != _maxTouristNumber) {
+                    _maxTouristNumber = value; OnPropertyChanged();
+                }
+            }
+        }
+
+        private double _duration;
+        public double Duration {
+            get { return _duration; }
+            set {
+                if (_duration != value) {
+                    _duration = value; OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _currentTouristNumber;
+        public int CurrentTouristNumber {
+            get { return _currentTouristNumber; }
+            set {
+                if (_currentTouristNumber != value) {
+                    _currentTouristNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public List<string> ProfilePictures { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Model/Tour.cs
+++ Model/Tour.cs
@@ -7,20 +7,62 @@ using System.Threading.Tasks;
 namespace BookingApp.Model {
     public class Tour : BookingApp.Serializer.ISerializable {
         public int Id { get; set; }
-        public Location Location { get; set; }
-        public double Duration { get; set; }
+        public string Name { get; set; }
+        public int LocationId { get; set; }
+        public string Description { get; set; }
         public string Language { get; set; }
         public int MaxTouristNumber { get; set; }
+        public List<PointOfInterest> PointOfInterests { get; set; }
+        public List<DateTime> Beginnings { get; set; }
+        public double Duration { get; set; }
         public int CurrentTouristNumber { get; set; }
-        public Tour() { }
+        public List<string> ProfilePictures { get; set; }
 
-        public void FromCSV(string[] values) {
-            
+        public Tour() { }
+        public Tour(int id, string name, int locationId, string description, string language, int maxTouristNumber, double duration, int currentTouristNumber, List<string> profilePictures) {
+            Id = id;
+            Name = name;
+            LocationId = locationId;
+            Description = description;
+            Language = language;
+            MaxTouristNumber = maxTouristNumber;
+            Duration = duration;
+            CurrentTouristNumber = currentTouristNumber;
+            ProfilePictures = profilePictures;
         }
 
         public string[] ToCSV() {
-            string[] values = { };
-            return values;
+            string[] csvValues = {
+                Id.ToString(),
+                Name,
+                LocationId.ToString(),
+                Description,
+                Language,
+                MaxTouristNumber.ToString(),
+                Duration.ToString(),
+                CurrentTouristNumber.ToString()
+                };
+
+             if (ProfilePictures != null) {
+                foreach (string profilePicture in ProfilePictures) {
+                    Array.Resize(ref csvValues, csvValues.Length + 1);
+                    csvValues[csvValues.Length - 1] = profilePicture;
+                }
+            }
+
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values) {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+            LocationId = int.Parse(values[2]);
+            Description = values[3];
+            Language = values[4];
+            MaxTouristNumber = Convert.ToInt32(values[5]);
+            Duration = Convert.ToDouble(values[6]);
+            CurrentTouristNumber = Convert.ToInt32(values[7]);
+            ProfilePictures.AddRange(values[8..]);
         }
     }
 }
