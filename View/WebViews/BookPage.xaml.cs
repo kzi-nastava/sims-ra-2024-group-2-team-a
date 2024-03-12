cf5d5cfb264074ad84004e017bd705776e3b12f1
nkusla <nikola.kuslakovic1616@gmail.com>
2024-03-12 12:16:09 +0100

    feat: Added GuestMainWindow and other pages
    - BookPage shows apartments in tickes
    - ReservationPage is empty atm
    - Refactored SignInForm

---
+++ View/WebViews/BookPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using BookingApp.DTO;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.WebViews {
+    /// <summary>
+    /// Interaction logic for BookPage.xaml
+    /// </summary>
+    public partial class BookPage : UserControl {
+
+        private ObservableCollection<AccommodationDTO> _accommodationDTOs = new ObservableCollection<AccommodationDTO>();
+        private AccommodationRepository _accommodationRepository = new AccommodationRepository();
+
+        public BookPage() {
+
+            InitializeComponent();
+
+            Update();
+
+            itemsControlAccommodations.ItemsSource = _accommodationDTOs;
+        }
+
+        public void Update() {
+            var accommodations = _accommodationRepository.GetAll();
+
+            _accommodationDTOs = new ObservableCollection<AccommodationDTO>(
+                accommodations.Select(a => new AccommodationDTO(a))
+                );
+        }
+    }
+}
---
+++ View/WebViews/GuestMainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.WebViews {
+    /// <summary>
+    /// Interaction logic for GuestMainWindow.xaml
+    /// </summary>
+    public partial class GuestMainWindow : Window {
+        public GuestMainWindow() {
+            InitializeComponent();
+            SetWindowProperties();
+            SetActiveUserControl(bookPage);
+        }
+
+        private void SetWindowProperties() {
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void SetActiveUserControl(UserControl userControl) {
+            bookPage.Visibility = Visibility.Collapsed;
+            reservationsPage.Visibility = Visibility.Collapsed;
+
+            userControl.Visibility = Visibility.Visible;
+        }
+
+        private void buttonBook_Click(object sender, RoutedEventArgs e) {
+            SetActiveUserControl(bookPage);
+        }
+
+        private void buttonReservations_Click(object sender, RoutedEventArgs e) {
+            SetActiveUserControl(reservationsPage);
+        }
+    }
+}
