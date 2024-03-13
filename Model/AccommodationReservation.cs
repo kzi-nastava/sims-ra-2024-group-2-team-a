dfb8f321cecb8d3353f1e73620c641fd6edbc0c0
nkusla <nikola.kuslakovic1616@gmail.com>
2024-03-13 13:30:54 +0100

    feat: Implemented accommodation reservation repo

--- Model/AccommodationReservation.cs
+++ Model/AccommodationReservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookingApp.Serializer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BookingApp.Model {
 
-    public class AccommodationReservation {
+    public class AccommodationReservation : ISerializable {
         
         public int Id { get; set; }
         public int IdGuest { get; set; }
