c1cc431d0376dfbeaa534e4fcc9355f858c3a12f
nkusla <nikola.kuslakovic1616@gmail.com>
2024-03-13 12:51:20 +0100

    refac: Refactored property names in Accommodation Reservation class

--- Model/AccommodationReservation.cs
+++ Model/AccommodationReservation.cs
@@ -11,22 +11,22 @@ namespace BookingApp.Model {
         public int Id { get; set; }
         public int IdGuest { get; set; }
         public int IdAccommodation { get; set; }
-        public int numberOfPersons { get; set; }
-        public DateOnly startDate { get; set; }
-        public DateOnly endDate { get; set; }
+        public int GuestsNumber { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
 
         public AccommodationReservation() { }
 
-        public AccommodationReservation(int idGuest, int idAccommodation, int numberOfPersons, DateOnly startDate, DateOnly endDate) {
+        public AccommodationReservation(int idGuest, int idAccommodation, int guestsNumber, DateOnly startDate, DateOnly endDate) {
             IdGuest = idGuest;
             IdAccommodation = idAccommodation;
-            this.numberOfPersons = numberOfPersons;
-            this.startDate = startDate;
-            this.endDate = endDate;
+            GuestsNumber = guestsNumber;
+            StartDate = startDate;
+            EndDate = endDate;
         }
 
         public string[] ToCSV() {
-            string[] csvValues = { Id.ToString(), IdGuest.ToString(), IdAccommodation.ToString(), numberOfPersons.ToString(), startDate.ToString("dd-MM-yyyy"), endDate.ToString("dd-MM-yyyy") };
+            string[] csvValues = { Id.ToString(), IdGuest.ToString(), IdAccommodation.ToString(), GuestsNumber.ToString(), StartDate.ToString("dd-MM-yyyy"), EndDate.ToString("dd-MM-yyyy") };
             return csvValues;
         }
 
@@ -34,9 +34,9 @@ namespace BookingApp.Model {
             Id = Convert.ToInt32(values[0]);
             IdGuest = Convert.ToInt32(values[1]);
             IdAccommodation = Convert.ToInt32(values[2]);
-            numberOfPersons = Convert.ToInt32(values[3]);
-            startDate = DateOnly.Parse(values[4]);
-            endDate = DateOnly.Parse(values[5]);
+            GuestsNumber = Convert.ToInt32(values[3]);
+            StartDate = DateOnly.Parse(values[4]);
+            EndDate = DateOnly.Parse(values[5]);
         }
     }
 }
