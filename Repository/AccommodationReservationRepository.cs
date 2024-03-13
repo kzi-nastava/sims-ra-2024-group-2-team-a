3152f7c3a8aeb53bae61c156143d5b4a544a4ba2
nkusla <nikola.kuslakovic1616@gmail.com>
2024-03-13 17:47:22 +0100

    refac: Refactored all repos so they inherit generic repo

--- Repository/AccommodationReservationRepository.cs
+++ Repository/AccommodationReservationRepository.cs
@@ -8,59 +8,7 @@ using System.Threading.Tasks;
 
 namespace BookingApp.Repository
 {
-    class AccommodationReservationRepository : IRepository<AccommodationReservation> {
+    class AccommodationReservationRepository : Repository<AccommodationReservation> {
 
-        private readonly Serializer<AccommodationReservation> _serializer;
-
-        private List<AccommodationReservation> _accommodationReservations;
-
-        public AccommodationReservationRepository() {
-            _serializer = new Serializer<AccommodationReservation>();
-        }
-
-        public bool Delete(AccommodationReservation item) {
-            throw new NotImplementedException();
-        }
-
-        public List<AccommodationReservation> GetAll() {
-            return _serializer.FromCSV();
-        }
-
-        public AccommodationReservation GetById(int id) {
-            _accommodationReservations = _serializer.FromCSV();
-            AccommodationReservation reservation = _accommodationReservations.Find(c => c.Id == id);
-
-            return reservation;
-        }
-
-        public AccommodationReservation Save(AccommodationReservation item) {
-            item.Id = NextId();
-            _accommodationReservations = _serializer.FromCSV();
-            _accommodationReservations.Add(item);
-            _serializer.ToCSV(_accommodationReservations);
-            return item;
-        }
-
-        public bool Update(AccommodationReservation item) {
-            AccommodationReservation reservation = this.GetById(item.Id);
-
-            if (reservation == null)
-                return false;
-
-            int index = _accommodationReservations.IndexOf(reservation);
-            _accommodationReservations.Remove(reservation);
-            _accommodationReservations.Insert(index, reservation);
-
-            _serializer.ToCSV(_accommodationReservations);
-            return true;
-        }
-
-        public int NextId() {
-            _accommodationReservations = _serializer.FromCSV();
-            if (_accommodationReservations.Count < 1)
-                return 1;
-
-            return _accommodationReservations.Max(c => c.Id) + 1;
-        }
     }
 }
--- Repository/TourRepository.cs
+++ Repository/TourRepository.cs
@@ -8,70 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BookingApp.Repository {
-    public class TourRepository : IRepository<Tour> {
-        private readonly Serializer<Tour> _serializer;
-
-        private List<Tour> _tours;
-        
-        public TourRepository() {
-            _serializer = new Serializer<Tour>();
-            _tours = _serializer.FromCSV();
-        }
-
-        public List<Tour> GetAll() {
-            return _serializer.FromCSV();
-        }
-
-        public Tour Save(Tour tour) {
-            tour.Id = NextId();
-            _tours = _serializer.FromCSV();
-            _tours.Add(tour);
-            _serializer.ToCSV(_tours);
-            return tour;
-        }
-
-        public bool Delete(Tour tour) {
-            _tours = _serializer.FromCSV();
-            Tour? found = _tours.Find(c => c.Id == tour.Id);
-            if(found == null) return false;
-
-            _tours.Remove(found);
-            _serializer.ToCSV(_tours);
-            return true;
-        }
-
-        public bool Update(Tour tour) {
-            _tours = _serializer.FromCSV();
-            Tour? current = _tours.Find(c => c.Id == tour.Id);
-            int index = _tours.IndexOf(current);
-            if(current == null) return false;
-
-            _tours.Remove(current);
-            _tours.Insert(index, tour);
-            _serializer.ToCSV(_tours);
-
-            return true;
-        }
-
-        public Tour GetById(int id) {
-            return _tours.Find(x => x.Id == id);
-        }
-
-        public int NextId() {
-            _tours = _serializer.FromCSV();
-            if (_tours.Count < 1) {
-                return 1;
-            }
-            return _tours.Max(c => c.Id) + 1;
-        }
+    public class TourRepository : Repository<Tour> {
 
         public List<Tour> GetFiltered(TourFilterDTO filter) {
-            _tours = _serializer.FromCSV();
+            _items = GetAll();
 
             if (filter.isEmpty())
-                return _tours;
+                return _items;
 
-            return _tours.Where(t => IsFiltered(t, filter)).ToList();
+            return _items.Where(t => IsFiltered(t, filter)).ToList();
         }
 
         private bool IsFiltered(Tour tour, TourFilterDTO filter) {
