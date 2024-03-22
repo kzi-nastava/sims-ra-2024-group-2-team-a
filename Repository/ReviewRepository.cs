fef017f929999d4a22adf56abefd192bdcfc4588
djukic002 <strahinjadjukic95@gmail.com>
2024-03-22 17:17:57 +0100

    feat: Review and SuperOwner
    -Added SuperOwner logic in OwnerRepository
    -Added some UI elements to SideMenuPage

--- Repository/ReviewRepository.cs
+++ Repository/ReviewRepository.cs
@@ -1,4 +1,5 @@
-﻿using BookingApp.Model;
+﻿using BookingApp.DTO;
+using BookingApp.Model;
 using BookingApp.Serializer;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,19 @@ namespace BookingApp.Repository {
         public Review GetByReservationId(int id) {
             return _serializer.FromCSV().Find(x=> x.ReservationId == id);
         }
+
+        public List<Review> GetByOwnerId(int ownerId) {
+            return _serializer.FromCSV().Where(review => review.OwnerId == ownerId).ToList();
+        }
+        public void GradeOwner(ReviewDTO reviewDTO) {
+            
+            
+            // prostor za bata kuleta
+            // veoma je bitno da se pozove tvoj save pre moje logike
+            // da se uracuna i ta najnovija ocena
+            OwnerRepository ownerRepository = new OwnerRepository();
+            ownerRepository.AdjustSuperOwner(reviewDTO.OwnerId);
+        }
+
     }
 }
