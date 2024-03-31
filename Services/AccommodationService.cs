215a960e7e648c4319168d9f5d936fc7391a5ca7
nkusla <nikola.kuslakovic1616@gmail.com>
2024-04-01 00:08:10 +0200

    feat: Created services layer
    - Created services for accommodation, reservation and reschedule request entities
    - Methods copied from repo classes to new service class

---
+++ Services/AccommodationService.cs
@@ -0,0 +1,36 @@
+﻿using BookingApp.DTO;
+using BookingApp.Model;
+using BookingApp.Repository;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Services
+{
+    class AccommodationService
+    {
+        private readonly AccommodationRepository _accommodationRepository = new AccommodationRepository();
+
+        public List<Accommodation> GetByOwnerId(int ownerId) {
+            return _accommodationRepository.GetByOwnerId(ownerId);
+        }
+
+        public List<Accommodation> GetFilteredAccommodations(AccommodationFilterDTO filter) {
+            var accommodations = _accommodationRepository.GetAll();
+
+            return accommodations.Where(a => SatisfiesFilter(a, filter)).ToList();
+        }
+
+        private bool SatisfiesFilter(Accommodation accommodation, AccommodationFilterDTO filter) {
+
+            return filter.MatchesName(accommodation.Name)
+                && filter.MatchesLocation(accommodation.LocationId)
+                && filter.MatchesType(accommodation.Type)
+                && filter.MatchesGuestNumber(accommodation.MaxGuestNumber)
+                && filter.MatchesReservationDays(accommodation.MinReservationDays);
+        }
+    }
+}
---
+++ Services/RescheduleRequestService.cs
@@ -0,0 +1,27 @@
+﻿using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Services
+{
+    class RescheduleRequestService
+    {
+        private readonly RescheduleRequestRepository _requestRepository = new RescheduleRequestRepository();
+
+        public List<RescheduleRequest> GetPendingRequestsByOwnerId(int ownerId) {
+            return _requestRepository.GetPendingRequestsByOwnerId(ownerId);
+        }
+
+        public List<RescheduleRequest> GetByReservationId(int reservationId) {
+            return _requestRepository.GetByReservationId(reservationId);
+        }
+
+        public List<RescheduleRequest> GetByGuestId(int guestId) {
+            return _requestRepository.GetByGuestId(guestId);
+        }
+    }
+}
