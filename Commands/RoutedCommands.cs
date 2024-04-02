1bf24edf6af52dbee7351453f30d3ddded45a4df
djukic002 <strahinjadjukic95@gmail.com>
2024-04-02 13:47:22 +0200

    feat: Reschedule Request Commands,Buttons logic
    -Added commands instead of events for 2 Buttons
    -Added Button disabled logic through commands

---
+++ Commands/RoutedCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BookingApp.Commands {
+    public static class RoutedCommands {
+
+        public static readonly RoutedUICommand AcceptButtonCommand = new RoutedUICommand(
+            "Disables accept/decline button",
+            "AcceptButtonCommand",
+            typeof(RoutedCommands),
+            null
+            );
+
+        public static readonly RoutedUICommand DeclineButtonCommand = new RoutedUICommand(
+            "Disables accept/decline button",
+            "DeclineButtonCommand",
+            typeof(RoutedCommands),
+            null
+            );
+    }
+}
