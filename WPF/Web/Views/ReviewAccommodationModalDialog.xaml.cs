e976e626e8b62812e9c004a5ddca4ebfb793366b
nkusla <nikola.kuslakovic1616@gmail.com>
2024-04-12 15:10:53 +0200

    refac: Added view model for Review accommodation modal dialog

--- WPF/Web/Views/ReviewAccommodationModalDialog.xaml.cs
+++ WPF/Web/Views/ReviewAccommodationModalDialog.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BookingApp.DTO;
 using BookingApp.Model;
 using BookingApp.Services;
+using BookingApp.WPF.Web.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,24 +23,15 @@ namespace BookingApp.WPF.Web.Views {
     /// </summary>
     public partial class ReviewAccommodationModalDialog : UserControl {
 
-        public AccommodationReservationDTO Reservation { get; set; }
-        public ImportanceType[] ImportanceTypes { get; set; } = (ImportanceType[]) Enum.GetValues(typeof(ImportanceType));
-        public ReviewDTO Review { get; set; } = new ReviewDTO();
+        private ReviewAccommodationModalDialogViewModel ViewModel { get; set; }
 
         private ReservationsPage _parentPage;
 
-        private readonly ReviewService _reviewService = new ReviewService();
-
         public ReviewAccommodationModalDialog(ReservationsPage parentPage, AccommodationReservationDTO reservation) {
             InitializeComponent();
-            Reservation = reservation;
             _parentPage = parentPage;
-            DataContext = this;
-
-            Review.ReservationId = Reservation.Id;
-            Review.GuestId = Reservation.GuestId;
-            Review.OwnerId = Reservation.Accommodation.OwnerId;
-            comboBoxRenovationImportance.SelectedIndex = 0;
+            ViewModel = new ReviewAccommodationModalDialogViewModel(reservation);
+            DataContext = ViewModel;
         }
 
         private void ButtonCancelClick(object sender, RoutedEventArgs e) {
@@ -47,7 +39,7 @@ namespace BookingApp.WPF.Web.Views {
         }
 
         private void ButtonConfirmClick(object sender, RoutedEventArgs e) {
-            _reviewService.GradeOwner(Review);
+            ViewModel.GradeOwner();
             _parentPage.CloseModalDialog();
         }
 
