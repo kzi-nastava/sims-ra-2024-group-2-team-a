a318091082cfa7e13a2d6e4d2204134661a4c828
nkusla <nikola.kuslakovic1616@gmail.com>
2024-04-13 14:42:35 +0200

    refac: Refactored branchign in SingInForm code-behind

--- WPF/SignInForm.xaml.cs
+++ WPF/SignInForm.xaml.cs
@@ -1,87 +1,77 @@
 ﻿using BookingApp.Model;
-using BookingApp.Repository;
-using BookingApp.WPF.Web.Views;
+using BookingApp.Services;
+using BookingApp.WPF.Android.Views;
 using BookingApp.WPF.Desktop.Views;
 using BookingApp.WPF.Tablet.Views;
+using BookingApp.WPF.Web.Views;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-using BookingApp.WPF.Android.Views;
 
-namespace BookingApp.WPF
-{
+namespace BookingApp.WPF {
     /// <summary>
     /// Interaction logic for SignInForm.xaml
     /// </summary>
-    public partial class SignInForm : Window
-    {
+    public partial class SignInForm : Window {
 
-        private readonly UserRepository _repository;
+        private readonly UserService _userService = new UserService();
 
         private string _username;
-        public string Username
-        {
+        public string Username {
             get => _username;
-            set
-            {
-                if (value != _username)
-                {
+            set {
+                if (value != _username) {
                     _username = value;
                     OnPropertyChanged();
                 }
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public SignInForm() {
+            InitializeComponent();
+            DataContext = this;
+        }
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
+        private void SignIn(object sender, RoutedEventArgs e) {
+            User user = _userService.GetByUsername(Username);
+
+            if (user == null) {
+                MessageBox.Show("Wrong username or password!");
+                return;
+            }
+
+            if (user.Password != passwordBox.Password) {
+                MessageBox.Show("Wrong username or password!");
+                return;
+            }
+
+            if (user.Category == UserCategory.Owner) {
+                MainWindow mainWindow = new MainWindow(user);
+                mainWindow.Show();
+                Close();
+            }
+            else if (user.Category == UserCategory.Tourist) {
+                TouristMainWindow touristMainWindow = new TouristMainWindow(user.Id);
+                touristMainWindow.Show();
+                Close();
+            }
+            else if (user.Category == UserCategory.Guide) {
+                GuideMainWindow guideMainWindow = new GuideMainWindow(user.Id);
+                guideMainWindow.Show();
+                Close();
+            }
+            else {
+                GuestMainWindow guestMainWindwo = new GuestMainWindow(user);
+                guestMainWindwo.Show();
+                Close();
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public SignInForm()
-        {
-            InitializeComponent();
-            DataContext = this;
-            _repository = new UserRepository();
-        }
-
-        private void SignIn(object sender, RoutedEventArgs e)
-        {
-            User user = _repository.GetByUsername(Username);
-            if (user != null)
-            {
-                if(user.Password == txtPassword.Password)
-                {
-                    if (user.Category == UserCategory.Owner)
-                    {
-                        MainWindow mainWindow = new MainWindow(user);
-                        mainWindow.Show();
-                        this.Close();
-                    }else if(user.Category == UserCategory.Tourist){
-                        TouristMainWindow touristMainWindow = new TouristMainWindow(user.Id);
-                        touristMainWindow.Show();
-                        this.Close();
-                    }else if(user.Category == UserCategory.Guide) {
-                        GuideMainWindow guideMainWindow = new GuideMainWindow(user.Id);
-                        guideMainWindow.Show();
-                        this.Close();
-                    }else{
-                        GuestMainWindow  guestMainWindwo = new GuestMainWindow(user);
-                        guestMainWindwo.Show();
-                        Close();
-                    }
-                } 
-                else
-                {
-                    MessageBox.Show("Wrong password!");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Wrong username!");
-            }
-            
-        }
     }
 }
