16a01e46b8efc6841dfab08acc8abf46c13d7b25
djukic002 <strahinjadjukic95@gmail.com>
2024-04-16 02:22:27 +0200

    feat: Renovation
    -Added whole backend infrastructure
    -Modified AccommodationRenovationPage

---
+++ Services/AccommodationRenovationService.cs
@@ -0,0 +1,18 @@
+﻿using BookingApp.Domain.Model;
+using BookingApp.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Services {
+    public class AccommodationRenovationService {
+        private readonly IAccommodationRenovationRepository _renovationRepository =
+            RepositoryInjector.GetInstance<IAccommodationRenovationRepository>();
+
+        public AccommodationRenovation GetById(int id) {
+            return _renovationRepository.GetById(id);
+        }
+    }
+}
