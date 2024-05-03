916740c650398dc1ddd8418d9b268d266a650ce8
lostojicc <ostojiclukaftn@gmail.com>
2024-05-04 01:53:43 +0200

    feat: Random

--- Services/TourRequestService.cs
+++ Services/TourRequestService.cs
@@ -5,6 +5,7 @@ using BookingApp.WPF.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,7 @@ namespace BookingApp.Services {
 
         private void UpdateRequestStatus() {
             foreach (TourRequest tourRequest in _tourRequestRepository.GetAll()) { 
-                if ((tourRequest.StartDate.ToDateTime(TimeOnly.Parse("00:00 AM")) - DateTime.Now).TotalHours < 48) {
+                if ((tourRequest.StartDate.ToDateTime(TimeOnly.Parse("00:00 AM")) - DateTime.Now).TotalHours < 48 && tourRequest.Status == TourRequestStatus.OnHold) {
                     tourRequest.Status = TourRequestStatus.Expired;
                     _tourRequestRepository.Update(tourRequest);
                 }
@@ -32,6 +33,39 @@ namespace BookingApp.Services {
 
         public void CreateRequest(TourRequestDTO tourRequestDTO, int passengerNumber) {
             _tourRequestRepository.Save(new TourRequest(tourRequestDTO, passengerNumber));
+        }
+
+        private List<TourRequest> GetAccepted(int userId) {
+            return GetByTouristId(userId).Where(r => r.Status == TourRequestStatus.Accepted).ToList();
+        }
+
+        public double GetAllTimeAcceptedPercentage(int userId) {
+            return ((double)GetAccepted(userId).Count() / (double)GetByTouristId(userId).Count());
+        }
+
+        public double GetAllTimeNotAcceptedPercentage(int userId) {
+            List<TourRequest> all = GetByTouristId(userId);
+            return (((double)all.Count() - (double)GetAccepted(userId).Count()) / (double)all.Count());
+        }
+
+        public double GetAllTimeAveragePassengerNumber(int userId) {
+            List<TourRequest> accepted = GetAccepted(userId);
+            return accepted.Sum(a => a.PassengerNumber) / (double)accepted.Count();
+        }
+
+        public double GetThisYearAcceptedPercentage(int userId) {
+            //return ((double)GetAccepted(userId).Where(r => r.).Count() / (double)GetByTouristId(userId).Count());
+            return 0;
+        }
+
+        public double GetThisYearNotAcceptedPercentage(int userId) {
+            List<TourRequest> all = GetByTouristId(userId);
+            return (((double)all.Count() - (double)GetAccepted(userId).Count()) / (double)all.Count());
+        }
+
+        public double GetThisYearAveragePassengerNumber(int userId) {
+            List<TourRequest> accepted = GetAccepted(userId);
+            return accepted.Sum(a => a.PassengerNumber) / (double)accepted.Count();
         }
     }
 }
---
+++ WPF/Desktop/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,109 @@
+﻿using BookingApp.Domain.RepositoryInterfaces;
+using BookingApp.Services;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BookingApp.WPF.Desktop.ViewModels
+{
+    public enum StatisticsPeriod { AllTime, ThisYear }
+    public class StatisticsViewModel : INotifyPropertyChanged
+    {
+        private readonly TourRequestService _tourRequestService = new TourRequestService(RepositoryInjector.GetInstance<ITourRequestRepository>());
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int UserId { get; set; }
+
+        public IEnumerable<StatisticsPeriod> StatisticsPeriodValues {
+            get { 
+                return Enum.GetValues(typeof(StatisticsPeriod)) as StatisticsPeriod[]; 
+            }
+        }
+
+        private StatisticsPeriod _period;
+        public StatisticsPeriod Period { 
+            get {
+                return _period;
+            }
+            set {
+                if (_period != value) {
+                    _period = value;
+                    OnPropertyChanged();
+                    LoadStatistics();
+                }                
+            }
+        }
+
+        private SeriesCollection _seriesCollection;
+        public SeriesCollection SeriesCollection {
+            get { return _seriesCollection; }
+            set {
+                _seriesCollection = value;
+                OnPropertyChanged(nameof(SeriesCollection));
+            }
+        }
+
+        private double _avgPeopleCount;
+        public double AvgPeopleCount {
+            get {
+                return _avgPeopleCount;
+            }
+            set {
+                if (value != _avgPeopleCount) {
+                    _avgPeopleCount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public StatisticsViewModel() { }
+        public StatisticsViewModel(int userId) { 
+            UserId = userId;
+            LoadStatistics();
+        }
+
+        private void LoadStatistics() {
+            SeriesCollection = new SeriesCollection();
+
+            switch (Period) {
+                case StatisticsPeriod.AllTime:
+                    LoadAllTime(SeriesCollection);
+                    break;
+                case StatisticsPeriod.ThisYear:
+                    LoadThisYear();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void LoadAllTime(SeriesCollection collection) {
+            collection.Clear();
+            collection.Add(new PieSeries {
+                Title = "Accepted",
+                Values = new ChartValues<double> { _tourRequestService.GetAllTimeAcceptedPercentage(UserId) }
+            });
+            collection.Add(new PieSeries {
+                Title = "Not accepted",
+                Values = new ChartValues<double> { _tourRequestService.GetAllTimeNotAcceptedPercentage(UserId) }
+            });
+            AvgPeopleCount = _tourRequestService.GetAllTimeAveragePassengerNumber(UserId);
+        }
+
+        private void LoadThisYear() {
+
+        }
+    }
+}
