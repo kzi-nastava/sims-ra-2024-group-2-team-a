58fc9c576908c038ccb787059cc3efdbd218bfd8
lostojicc <ostojiclukaftn@gmail.com>
2024-05-09 01:49:34 +0200

    feat: Finished statistics
    - finished functionality and looks

--- Services/TourRequestService.cs
+++ Services/TourRequestService.cs
@@ -12,14 +12,25 @@ using System.Threading.Tasks;
 namespace BookingApp.Services {
     public class TourRequestService {
         private readonly ITourRequestRepository _tourRequestRepository;
+        private LocationService _locationService;
+        private LanguageService _languageService;
 
         public TourRequestService(ITourRequestRepository tourRequestRepository) {
             _tourRequestRepository = tourRequestRepository;
             UpdateRequestStatus();
         }
 
+        public void InjectServices(LocationService locationService, LanguageService languageService) {
+            _locationService = locationService;
+            _languageService = languageService;
+        }
+
         public List<TourRequest> GetByTouristId(int id) { 
             return _tourRequestRepository.GetByTouristId(id); 
+        }
+
+        public List<TourRequest> GetByTouristIdThisYear(int id) {
+            return GetByTouristId(id).Where(a => a.EndDate.Year == DateOnly.FromDateTime(DateTime.Now).Year).ToList();
         }
 
         private void UpdateRequestStatus() {
@@ -39,6 +50,10 @@ namespace BookingApp.Services {
             return GetByTouristId(userId).Where(r => r.Status == TourRequestStatus.Accepted).ToList();
         }
 
+        public List<TourRequest> GetAcceptedThisYear(int userId) {
+            return GetByTouristIdThisYear(userId).Where(r => r.Status == TourRequestStatus.Accepted).ToList();
+        }
+
         public double GetAllTimeAcceptedPercentage(int userId) {
             return ((double)GetAccepted(userId).Count() / (double)GetByTouristId(userId).Count());
         }
@@ -54,18 +69,43 @@ namespace BookingApp.Services {
         }
 
         public double GetThisYearAcceptedPercentage(int userId) {
-            //return ((double)GetAccepted(userId).Where(r => r.).Count() / (double)GetByTouristId(userId).Count());
-            return 0;
+            return ((double)GetAcceptedThisYear(userId).Count() / (double)GetByTouristIdThisYear(userId).Count());
         }
 
         public double GetThisYearNotAcceptedPercentage(int userId) {
-            List<TourRequest> all = GetByTouristId(userId);
-            return (((double)all.Count() - (double)GetAccepted(userId).Count()) / (double)all.Count());
+            List<TourRequest> all = GetByTouristIdThisYear(userId);
+            return (((double)all.Count() - (double)GetAcceptedThisYear(userId).Count()) / (double)all.Count());
         }
 
         public double GetThisYearAveragePassengerNumber(int userId) {
-            List<TourRequest> accepted = GetAccepted(userId);
+            List<TourRequest> accepted = GetAcceptedThisYear(userId);
             return accepted.Sum(a => a.PassengerNumber) / (double)accepted.Count();
+        }
+
+        public int GetRequestNumberByLocation(Location location, int userId) {
+            return GetByTouristId(userId).Where(r => r.LocationId == location.Id).Count();
+        }
+
+        public int GetRequestNumberByLanguage(Language language, int userId) {
+            return GetByTouristId(userId).Where(r => r.LanguageId == language.Id).Count();
+        }
+
+        public Dictionary<Location, int> GetRequestsByLocations(int userId) {
+            Dictionary<Location, int> pairs = new Dictionary<Location, int>();
+
+            foreach(Location location in _locationService.GetAll()) 
+                pairs.Add(location, GetRequestNumberByLocation(location, userId));
+            
+            return pairs;
+        }
+
+        public Dictionary<Language, int> GetRequestsByLanguages(int userId) {
+            Dictionary<Language, int> pairs = new Dictionary<Language, int>();
+
+            foreach (Language language in _languageService.GetAll())
+                pairs.Add(language, GetRequestNumberByLanguage(language, userId));
+
+            return pairs;
         }
     }
 }
--- WPF/Desktop/ViewModels/StatisticsViewModel.cs
+++ WPF/Desktop/ViewModels/StatisticsViewModel.cs
@@ -1,22 +1,27 @@
 ﻿using BookingApp.Domain.RepositoryInterfaces;
 using BookingApp.Services;
 using LiveCharts;
+using LiveCharts.Defaults;
 using LiveCharts.Wpf;
+using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
 
 namespace BookingApp.WPF.Desktop.ViewModels
 {
     public enum StatisticsPeriod { AllTime, ThisYear }
     public class StatisticsViewModel : INotifyPropertyChanged
     {
-        private readonly TourRequestService _tourRequestService = new TourRequestService(RepositoryInjector.GetInstance<ITourRequestRepository>());
+        private readonly TourRequestService _tourRequestService = ServicesPool.GetService<TourRequestService>();
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -55,6 +60,24 @@ namespace BookingApp.WPF.Desktop.ViewModels
             }
         }
 
+        private SeriesCollection _locationCollection = new SeriesCollection();
+        public SeriesCollection LocationCollection {
+            get { return _locationCollection; }
+            set {
+                _locationCollection = value;
+                OnPropertyChanged(nameof(LocationCollection));
+            }
+        }
+
+        private SeriesCollection _languageCollection = new SeriesCollection();
+        public SeriesCollection LanguageCollection {
+            get { return _languageCollection; }
+            set {
+                _languageCollection = value;
+                OnPropertyChanged(nameof(LanguageCollection));
+            }
+        }
+
         private double _avgPeopleCount;
         public double AvgPeopleCount {
             get {
@@ -68,10 +91,51 @@ namespace BookingApp.WPF.Desktop.ViewModels
             }
         }
 
+        public List<SolidColorBrush> SolidColorBrushes { get; set; }
+
         public StatisticsViewModel() { }
         public StatisticsViewModel(int userId) { 
             UserId = userId;
+
+            SolidColorBrushes = new List<SolidColorBrush>();
+
+            // Retrieve SolidColorBrushes from the application resources
+            SolidColorBrushes.Add(Application.Current.Resources["HardGreen"] as SolidColorBrush);
+            SolidColorBrushes.Add(Application.Current.Resources["Green"] as SolidColorBrush);
+            SolidColorBrushes.Add(Application.Current.Resources["SoftGreen"] as SolidColorBrush);
+
             LoadStatistics();
+            LoadLocationStatistics();
+            LoadLanguageStatistics();
+        }
+
+        private void LoadLocationStatistics() {
+            int brushIndex = 0;
+
+            foreach (var pair in _tourRequestService.GetRequestsByLocations(UserId)) {
+                //LocationLabels.Add(pair.Key.City + " - " + pair.Key.Country);
+                LocationCollection.Add(new RowSeries {
+                    Title = pair.Key.City + " - " + pair.Key.Country,
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(pair.Value) },
+                    Fill = SolidColorBrushes[brushIndex %  SolidColorBrushes.Count]
+                });  
+
+                brushIndex++;
+            }
+        }
+
+        private void LoadLanguageStatistics() {
+            int brushIndex = 0;
+
+            foreach (var pair in _tourRequestService.GetRequestsByLanguages(UserId)) {
+                LanguageCollection.Add(new RowSeries {
+                    Title = pair.Key.Name,
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(pair.Value) },
+                    Fill = SolidColorBrushes[brushIndex % SolidColorBrushes.Count]
+                });
+
+                brushIndex++;
+            }
         }
 
         private void LoadStatistics() {
@@ -82,7 +146,7 @@ namespace BookingApp.WPF.Desktop.ViewModels
                     LoadAllTime(SeriesCollection);
                     break;
                 case StatisticsPeriod.ThisYear:
-                    LoadThisYear();
+                    LoadThisYear(SeriesCollection);
                     break;
                 default:
                     break;
@@ -93,17 +157,30 @@ namespace BookingApp.WPF.Desktop.ViewModels
             collection.Clear();
             collection.Add(new PieSeries {
                 Title = "Accepted",
-                Values = new ChartValues<double> { _tourRequestService.GetAllTimeAcceptedPercentage(UserId) }
+                Values = new ChartValues<double> { _tourRequestService.GetAllTimeAcceptedPercentage(UserId) },
+                Fill = SolidColorBrushes[0]
             });
             collection.Add(new PieSeries {
                 Title = "Not accepted",
-                Values = new ChartValues<double> { _tourRequestService.GetAllTimeNotAcceptedPercentage(UserId) }
+                Values = new ChartValues<double> { _tourRequestService.GetAllTimeNotAcceptedPercentage(UserId) },
+                Fill = SolidColorBrushes[2]
             });
             AvgPeopleCount = _tourRequestService.GetAllTimeAveragePassengerNumber(UserId);
         }
 
-        private void LoadThisYear() {
-
+        private void LoadThisYear(SeriesCollection collection) {
+            collection.Clear();
+            collection.Add(new PieSeries {
+                Title = "Accepted",
+                Values = new ChartValues<double> { _tourRequestService.GetThisYearAcceptedPercentage(UserId) },
+                Fill = SolidColorBrushes[0]
+            });
+            collection.Add(new PieSeries {
+                Title = "Not accepted",
+                Values = new ChartValues<double> { _tourRequestService.GetThisYearNotAcceptedPercentage(UserId) },
+                Fill = SolidColorBrushes[2]
+            });
+            AvgPeopleCount = _tourRequestService.GetThisYearAveragePassengerNumber(UserId);
         }
     }
 }
