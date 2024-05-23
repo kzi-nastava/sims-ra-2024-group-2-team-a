45ec4d1a115eb398af23ebb167e2c3a2376b9a6b
djukic002 <strahinjadjukic95@gmail.com>
2024-05-23 12:07:52 +0200

    feat: Owner report generation finished
    -Added PdfGenerationWindow

--- WPF/Android/Views/AccommodationPage.xaml.cs
+++ WPF/Android/Views/AccommodationPage.xaml.cs
@@ -4,13 +4,10 @@ using BookingApp.WPF.DTO;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using QuestPDF.Infrastructure;
-using QuestPDF.Fluent;
-using QuestPDF.Helpers;
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel;
-using IContainer = QuestPDF.Infrastructure.IContainer;
+
 
 namespace BookingApp.WPF.Android.Views {
     /// <summary>
@@ -92,87 +89,8 @@ namespace BookingApp.WPF.Android.Views {
         }
 
         private void GeneratePdfButton_Click(object sender, RoutedEventArgs e) {
-            QuestPDF.Settings.License = LicenseType.Community;
-
-            Document.Create(container => {
-                container.Page(page => {
-                    page.Margin(2, Unit.Centimetre);
-                    page.Size(PageSizes.A4);
-                    page.DefaultTextStyle(x => x.FontSize(14));
-                    page.Header().Text("Average grades for your accommodations")
-                    .SemiBold().FontSize(20)
-                    .FontColor(Colors.Black).AlignCenter();
-
-                    page.Content().Element(ComposeContent);
-                });
-            }).GeneratePdf("C:/Users/Strahinja/Desktop/niggers.pdf");
+            PdfGenerationWindow pdfGenerationWindow = new PdfGenerationWindow(_user);
+            pdfGenerationWindow.ShowDialog();
         }
-
-        void ComposeContent(IContainer container) {
-            container
-                .PaddingVertical(20)
-                .Background(Colors.Grey.Lighten3)
-                .AlignTop()
-                .AlignCenter().Column
-                    (column =>
-                {
-                    column.Spacing(5);
-                    column.Item().Text(accommodationType.ToString().ToUpper()).AlignCenter().Underline(true).Bold();
-                    column.Item().Element(ComposeTable);
-
-                    accommodationType = AccommodationType.house;
-                    column.Item().PaddingTop(20).Text(accommodationType.ToString().ToUpper()).AlignCenter().Underline(true).Bold();
-                    column.Item().Element(ComposeTable);
-
-                    accommodationType = AccommodationType.hut;
-                    column.Item().PaddingTop(20).Text(accommodationType.ToString().ToUpper()).AlignCenter().Underline(true).Bold();
-                    column.Item().Element(ComposeTable);
-                });
-        }
-
-        private AccommodationType accommodationType = AccommodationType.apartment;
-
-        void ComposeTable(IContainer container) {
-            int id = 1;
-            Dictionary<Accommodation, double> data = _reviewService.GetAverageAccommodationGradesByOwnerId(_user.Id, accommodationType);
-
-            container
-                .Table(table => {
-                    table.ColumnsDefinition(columns => {
-                        columns.ConstantColumn(25);
-                        columns.RelativeColumn(2);
-                        columns.RelativeColumn(2);
-                        columns.RelativeColumn();
-                    });
-
-
-                    table.Header(header => {
-                        header.Cell().Element(CellStyle).Text("#");
-                        header.Cell().Element(CellStyle).Text("Accommodation name");
-                        header.Cell().Element(CellStyle).Text("Location");
-                        header.Cell().Element(CellStyle).AlignCenter().Text("Average");
-
-                        static IContainer CellStyle(IContainer container) {
-                            return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
-                        }
-                    });
-
-                    foreach (var item in data) {
-                        table.Cell().Element(CellStyle).Text(id.ToString());
-                        id++;
-                        table.Cell().Element(CellStyle).Text(item.Key.Name);
-
-                        LocationDTO locationDTO = new LocationDTO(locationService.GetById(item.Key.LocationId));
-
-                        table.Cell().Element(CellStyle).Text(locationDTO.LocationInfoTemplate);
-                        table.Cell().Element(CellStyle).AlignCenter().Text(System.Math.Round(item.Value, 2).ToString());
-
-                        static IContainer CellStyle(IContainer container) {
-                            return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-                        }
-                    }
-                });
-        }
-
     }
 }
